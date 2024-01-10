f40177f8fd9f0b1e7f096bc83f98d40e2ee921d7
Nnenty <>
2024-01-10 23:28:46 +0300

    add new file

---
+++ sum_sum_arr2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#define SIZE 10
+int sump(int *start, int *end);
+int main(void)
+{
+    int marbles[SIZE] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 10};
+    long answer;
+
+    answer = sump(marbles, marbles + SIZE);
+    printf("Общаая сумма элементов массива marbles равна %ld.\n",
+           answer);
+
+    return 0;
+}
+int sump(int *start, int *end)
+{
+    int total = 0;
+
+    while (start < end)
+    {
+        total += *start;
+        start++;
+    }
+    return total;
+}
