5ea489397a99d21ea8ed85b46c098b3284f23e78
Nnenty <>
2024-01-25 00:14:06 +0300

    add new file

---
+++ fgets2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+#define STLEN 10
+
+int main(void)
+{
+    char words[STLEN];
+
+    puts("Введите строки (или пустую строку для выхода):");
+    while (fgets(words, STLEN, stdin) != NULL && words[0] != '\n')
+        ;
+    {
+        fputs(words, stdout);
+    }
+    puts("Готово.");
+
+    return 0;
+}
