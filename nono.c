ed2cfb2de467ee84589d40612bd310d8a16ac0f2
Nnenty <>
2024-01-27 18:24:17 +0300

    add new file

---
+++ nono.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char side_a[] = " Сторона А";
+    char dont[] = {'У', 'Р', 'А', '!'};
+    char side_b[] = "Сторона B";
+
+    puts(dont);
+
+    return 0;
+}
